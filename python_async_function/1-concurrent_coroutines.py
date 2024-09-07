030c1f0a72a2fabd0ac3898bb789f9afd9b2f801
PSHEMA <p.shema@alustudent.com>
2024-09-07 17:05:58 +0200

    Multiple coroutines

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,5 +10,17 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ wait n """
-    coroutines = [wait_random(max_delay) for _ in range(n)]
-    return [await coro for coro in gather(*coroutines)]
+    tasks = [wait_random(max_delay) for x in range(n)]
+    data_list = await gather(*tasks)
+    new_list = []
+
+    while data_list:
+        minimum = data_list[0]
+        for x in data_list:
+            if x < minimum:
+                minimum = x
+        new_list.append(minimum)
+        data_list.remove(minimum)
+
+    return new_list
+    
