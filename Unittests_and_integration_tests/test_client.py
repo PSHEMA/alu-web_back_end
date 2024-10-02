f057c31b2bc88dbf36ba119d49c918d766a84c80
PSHEMA <p.shema@alustudent.com>
2024-10-02 19:08:14 +0200

    More patching

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -6,6 +6,7 @@ from parameterized import parameterized
 from client import GithubOrgClient
 from utils import get_json
 
+
 class TestGithubOrgClient(unittest.TestCase):
     """Test GithubOrgClient"""
 
@@ -13,13 +14,19 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",),
         ("abc",),
     ])
-    @patch('client.get_json', return_value={"repos_url": "https://api.github.com/orgs/google/repos"})
+    @patch('client.get_json', return_value={"repos_url":
+                                            "https://api.github.com/orgs/google/repos"
+                                            })
     def test_org(self, org_name, mock_get_json):
         """Test that GithubOrgClient.org returns the correct value"""
         client = GithubOrgClient(org_name)
         result = client.org
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
-        self.assertEqual(result, {"repos_url": f"https://api.github.com/orgs/{org_name}/repos"})
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
+        self.assertEqual(result, {"repos_url":
+                                  f"https://api.github.com/orgs/{org_name}/repos"
+                                  })
+    
     
     def test_public_repos_url(self):
         """Test the _public_repos_url property"""
@@ -28,6 +35,7 @@ class TestGithubOrgClient(unittest.TestCase):
             client = GithubOrgClient("google")
             result = client._public_repos_url
             self.assertEqual(result, "https://api.github.com/orgs/google/repos")
+    
     @patch("client.get_json")
     def test_public_repos(self, mock):
         """ test public repos """
