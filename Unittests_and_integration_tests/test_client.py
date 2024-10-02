4937d7a24843561abf4070b238436702575b0b28
PSHEMA <p.shema@alustudent.com>
2024-10-02 19:24:27 +0200

    fictures

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,8 @@
 """ Unittests for client.py """
 import unittest
 from unittest.mock import patch
-from parameterized import parameterized
+from Unittests_and_integration_tests.fixtures import TEST_PAYLOAD
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
 from utils import get_json
 
@@ -59,6 +60,42 @@ class TestGithubOrgClient(unittest.TestCase):
         """Test has_license"""
         result = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(result, expected_result)
+
+
+@parameterized_class(('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'), TEST_PAYLOAD)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ Test integration """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Setup class """
+        org = TEST_PAYLOAD[0][0]
+        repos = TEST_PAYLOAD[0][1]
+        org_mock = Mock()
+        org_mock.json = Mock(return_value=org)
+        cls.org_mock = org_mock
+        repos_mock = Mock()
+        repos_mock.json = Mock(return_value=repos)
+        cls.repos_mock = repos_mock
+
+        cls.get_patcher = patch('requests.get')
+        cls.get = cls.get_patcher.start()
+
+        options = {cls.org_payload["repos_url"]: repos_mock}
+        cls.get.side_effect = lambda y: options.get(y, org_mock)
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Tear down class """
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """ Test public repos """
+        org = GithubOrgClient("test")
+        repos = org.public_repos()
+        payload = self.repos_payload
+        self.assertEqual(repos, self.expected_repos)
+        self.assertEqual(payload, self.repos_payload)
 
 
 if __name__ == "__main__":
