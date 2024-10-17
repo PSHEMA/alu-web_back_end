cff9a8efa508e5816f00d6102824d7d0eed52e35
PSHEMA <p.shema@alustudent.com>
2024-10-17 19:33:49 +0200

    Get locale from request

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -17,7 +17,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.default_locale
+@babel.localeselector
 def get_locale() -> str:
     """ get_locale """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
