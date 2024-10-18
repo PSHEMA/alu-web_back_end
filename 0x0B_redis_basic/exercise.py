664ab786fd5f29a656409dbe042c003dbd64339d
PSHEMA <p.shema@alustudent.com>
2024-10-18 19:52:28 +0200

    Retrieving Lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -10,7 +10,9 @@ def count_calls(method: Callable) -> Callable:
     """Decorator to count how many times a method is called."""
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """Wrapper function that increments the call count and invokes the original method."""
+        """Wrapper function that increments the call count
+            and invokes the original method.
+        """
         key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args, **kwargs)
@@ -18,10 +20,14 @@ def count_calls(method: Callable) -> Callable:
 
 
 def call_history(method: Callable) -> Callable:
-    """Decorator to store the history of inputs and outputs for a particular function."""
+    """Decorator to store the history of inputs
+        and outputs for a particular function.
+    """
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """Wrapper function that stores the input and output of the function."""
+        """Wrapper function that stores the input
+            and output of the function.
+        """
         key = method.__qualname__
         inputs = f"{key}:inputs"
         outputs = f"{key}:outputs"
@@ -52,7 +58,7 @@ class Cache:
         """ initialize redis connection and flush database """
         self._redis = redis.Redis()
         self._redis.flushdb()
-        
+    
     @count_calls
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
@@ -60,18 +66,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-    
+
     def get(self, key: str, fn: Optional[Callable] = None) -> Union[bytes, str, int, float]:
         """ get data from redis """
         data = self._redis.get(key)
         if fn:
             return fn(data)
         return data
-    
+
     def get_str(self, key: str) -> str:
         """ get string from redis """
         return self.get(key, lambda x: x.decode('utf-8'))
-    
+
     def get_int(self, key: str) -> int:
         """ get int from redis """
         return self.get(key, int)
