2f04985b70b4b09290ac552733c04ea45a754c4a
PSHEMA <p.shema@alustudent.com>
2024-10-23 23:28:22 +0200

    Average Score

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -3,11 +3,12 @@
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
+    IN input_user_id INT
 )
 BEGIN
     DECLARE average_score FLOAT;
-    SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
-    UPDATE users SET average_score = average_score WHERE id = user_id;
-END$$
-DELIMITER;
+    SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = input_user_id;
+    UPDATE users SET average_score = average_score WHERE id = input_user_id;
+END $$
+
+DELIMITER ;
