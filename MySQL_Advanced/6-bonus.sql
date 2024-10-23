d28fc98c728e8154b2c5e6f1515c25d3de6b6cb3
PSHEMA <p.shema@alustudent.com>
2024-10-23 23:14:18 +0200

    Add bonus

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -7,8 +7,12 @@ CREATE PROCEDURE AddBonus(
     IN score INT
 )
 BEGIN
-    INSERT IGNORE INTO projects(name) VALUES(project_name);
-    INSERT INTO corrections(user_id, project_id, score)
-    VALUES(user_id, (SELECT id FROM projects WHERE name = project_name), score);
+    DECLARE project_id INT;
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    IF project_id is NULL THEN
+        insert into projects (name, id) values (project_name, project_id);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END$$
 DELIMITER ;
