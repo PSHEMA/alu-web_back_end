6b2adc2bbe05cf93caaef7ed4772570a1a0f8bb4
PSHEMA <p.shema@alustudent.com>
2024-10-25 00:08:30 +0200

    a function that lists all documents in a collection

---
+++ NoSQL/8-all.py
@@ -0,0 +1,5 @@
+""" a Python function that lists all documents in a collection """
+
+def list_all(mongo_collection):
+    """ lists all documents in a collection """
+    return list(mongo_collection.find())
