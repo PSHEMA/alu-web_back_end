4af52cc00f0f8bf0df9b935bfed660afc7ecb935
PSHEMA <p.shema@alustudent.com>
2024-11-01 00:11:03 +0200

    method properties

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -5,8 +5,8 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   return {
-    income: [`income-${getCurrentYear()}`],
-    gdp: [`gdp-${getCurrentYear()}`],
-    capita: [`capita-${getCurrentYear()}`], 
-  }
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 }
