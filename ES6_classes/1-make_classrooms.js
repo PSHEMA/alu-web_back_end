deb6b92989812d8e2b41ba270c64c68641e12f97
PSHEMA <p.shema@alustudent.com>
2024-11-06 11:24:32 +0200

    makeSomeClassRooms

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -4,5 +4,5 @@ function initializeRooms() {
   const classRoom1 = new classRoom(19);
   const classRoom2 = new classRoom(20);
   const classRoom3 = new classRoom(34);
-  return [classRoom1, classRoom2];
+  return [classRoom1, classRoom2, classRoom3];
 }
