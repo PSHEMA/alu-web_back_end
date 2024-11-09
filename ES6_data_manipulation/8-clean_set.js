64fa55ba4bec43e29d28570c8ddc9fe61866d8b5
PSHEMA <p.shema@alustudent.com>
2024-11-09 18:53:12 +0200

    Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,8 @@
 export default function cleanSet(set, startString) {
-  return [...set].filter((value) => value.startsWith(startString)).join('-');
+  let result = '';
+  if (!startString || !startString.length) return result;
+  set.forEach((i) => {
+    if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
+  });
+  return result.slice(0, result.length - 1);
 }
