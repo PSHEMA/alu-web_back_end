febbd50e669bbb1c094607f917c57138efef5fe3
PSHEMA <p.shema@alustudent.com>
2024-11-13 21:23:23 +0200

    Express Http

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const app = express();
-port = 1245;
+const port = 1245;
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
-    // console.log(`Example app listening on port ${port}!`);
+  //   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+module.exports = app;
